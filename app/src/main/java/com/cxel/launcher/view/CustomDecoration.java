3bc19debebec8a42c638efe3bb9bb545d76a8fc7
andevele <andevele@163.com>
2019-10-05 20:42:53 +0800

    apps页面垂直间距、水平间距重新设置

--- app/src/main/java/com/cxel/launcher/view/CustomDecoration.java
+++ app/src/main/java/com/cxel/launcher/view/CustomDecoration.java
@@ -22,26 +22,28 @@ public class CustomDecoration extends RecyclerView.ItemDecoration {
 
         if (parent.getChildAdapterPosition(view) % 4 == 0) {
             //第一列
-            outRect.left = 0;
-            outRect.right = 15;
+            outRect.left = 90;
+            outRect.right = -30;
         } else if (parent.getChildAdapterPosition(view) % 4 == 1) {
             //第二列
-            outRect.left = 5;
-            outRect.right = 10;
+            outRect.left = 50;
+            outRect.right = 5;
         } else if (parent.getChildAdapterPosition(view) % 4 == 2) {
             //第三列
-            outRect.left = 10;
-            outRect.right = 5;
+            outRect.left = 15;
+            outRect.right = 30;
         } else if (parent.getChildAdapterPosition(view) % 4 == 3) {
             //第四列
-            outRect.left = 15;
-            outRect.right = 0;
+            outRect.left = -10;
+            outRect.right = 90;
         }
-//        if(visualPos - 4 < 0) {
-//            outRect.top = 20;
-//        } else {
+        if(visualPos - 4 < 0) {
+            outRect.top = 20;
+        }
+//        else {
 //            outRect.top = 20;
 //        }
+        outRect.bottom = 20;
 
     }
 }
