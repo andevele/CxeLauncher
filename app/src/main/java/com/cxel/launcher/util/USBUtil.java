d3f8a369adf9d89ebdfb21615f46706068561966
andevele <andevele@163.com>
2019-12-28 13:49:09 +0800

    添加获取USB信息的方法

--- app/src/main/java/com/cxel/launcher/util/USBUtil.java
+++ app/src/main/java/com/cxel/launcher/util/USBUtil.java
@@ -1,10 +1,13 @@
 package com.cxel.launcher.util;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
 import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * zhulf 20190924
@@ -35,6 +38,25 @@ public class USBUtil {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public static String getUSBPathInfo(StorageManager storageManager) {
+        String ret = "/mnt/sdcard";
+        try {
+            Class clz = StorageManager.class;
+            Method getVolumeList = clz.getMethod("getVolumeList");
+            StorageVolume[] volumes = (StorageVolume[]) getVolumeList.invoke(storageManager);
+            for (int i = 0; i < volumes.length; i++) {
+                String element = volumes[i].toString();
+                Log.d("zhulf","==element: " + element);
+                if (element.contains("/mnt/usb/s") && element.contains("mounted")) {
+                    return element;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ret;
     }
 
 
